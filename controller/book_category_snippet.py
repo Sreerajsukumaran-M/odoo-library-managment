54c94e4d4c253868b725fdfd78439555b3f96d59
Sreerajsukumaran-M <sreerajsukumaran2002@gmail.com>
2026-05-14 14:17:47 +0530

    first commit

---
+++ controller/book_category_snippet.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+from odoo import http
+from odoo.http import request
+
+class WebsiteProduct(http.Controller):
+
+   @http.route('/get_product_categories', auth="public", type='jsonrpc',website=True)
+   def get_product_category(self):
+       """Get the website categories for the snippet."""
+
+
+       categories = self.env[
+           'library.book'].search_read([('sale_count','>',0)],order='sale_count desc')
+       values = {
+           'categories': categories,
+       }
+       return values
---
+++ static/src/js/book_category.js
@@ -0,0 +1,35 @@
+/** @odoo-module */
+import { renderToElement } from "@web/core/utils/render";
+import publicWidget from "@web/legacy/js/public/public_widget";
+import { rpc } from "@web/core/network/rpc";
+
+publicWidget.registry.get_product_tab = publicWidget.Widget.extend({
+   selector : '.categories_section',
+   async willStart() {
+       const result = await rpc('/get_product_categories', {});
+       console.log("result=",result)
+       if(result && result.categories){
+           console.log("hello 12")
+           var chunks=[]
+           const data=result.categories;
+           for(let i=0 ;i<data.length;i+=4){
+               chunks.push(data.slice(i,i+4));
+           }
+           if (chunks.length >0){
+               chunks[0].is_active =true;
+           }
+           this.chunks = chunks;
+
+       }
+   },
+    start()
+        {const id_number=Math.random()
+        if (this.chunks)
+        {
+         this.$target.empty().html(renderToElement('library_management.category_data', {
+         result: this.chunks,
+             id_number :id_number,
+            }))
+        }
+    },
+});
